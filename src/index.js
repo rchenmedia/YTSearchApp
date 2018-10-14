2ed38843d5a81843ec966f2396335435c9988b6b
Richard Chen <rchenmedia@gmail.com>
2018-10-13 22:18:07 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyDDsKvwxgK9OnoeYA8TA7UOkS2vv_zxe6c';
+
+
+class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch('surfboards');
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+				});
+			});
+		}
+
+
+	render() {
+		//limits its triggering to every 300 milliseconds(throttling)
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+		return (
+			<div className='container-fluid'>
+				<SearchBar onSearchTermChange={videoSearch}/>
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+				  	videos={this.state.videos}/>
+			</div>
+			);
+		}
+	}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
